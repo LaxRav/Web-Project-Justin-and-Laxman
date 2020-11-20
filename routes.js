cc79d7bdd9f3af516ed0d225b4620ff651b44525
Laxman <181336x@mymail.nyp.edu.sg>
2020-11-20 10:49:29 +0800

    These are the changes to the project for now!!
    Further development needed

---
+++ routes.js
@@ -0,0 +1,27 @@
+var bodyParser = require('body-parser');
+
+    var routes = function() {
+        var router = require('express').Router();
+
+        router.use(bodyParser.urlencoded({
+   extended: true
+        }));
+
+        router.get('/', function(req, res) {
+     res.sendFile(__dirname + "/views/index.html");
+        });
+        
+
+        router.get('/css/*', function(req, res) {
+     res.sendFile(__dirname+"/views/"+req.originalUrl);
+        });
+
+         router.get('/js/*', function(req, res) {
+     res.sendFile(__dirname+"/views/"+req.originalUrl);
+        });
+        return router;
+
+    };
+
+    module.exports = routes();
+
