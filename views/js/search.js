483b952678eaa097e7e606fb4c3d9b49fb7e9363
LaxRav <65710248+LaxRav@users.noreply.github.com>
2021-01-11 16:50:31 +0800

    J&L

---
+++ views/js/search.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "/movies/search",
+        method: "post"
+    })
+        .done(
+            function (data) {
+                data.forEach (function(movies) {
+                    $(".super").append(`
+                    <article>
+                        <h2>${movies.movie}</h2>
+                        <div>
+                           Price: ${movies.price}<br>
+                           Description : ${movies.description}<br>
+                            Release: ${movies.release}<br>
+                            Distributor: ${movies.distributor}<br>
+                            Language: ${movies.language}<br>
+                        </div>
+                    </article>
+                `);
+                })
+            }
+        )
+
+        .fail(
+            function (err) {
+             console.log(err.responseText);
+            }
+  )
+
+
+        })
