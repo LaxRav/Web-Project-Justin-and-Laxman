828fab30f231b12bab6519b3b426ad1f011a0985
LaxRav <65710248+LaxRav@users.noreply.github.com>
2021-01-21 19:34:12 +0800

    changes

--- views/js/search.js
+++ views/js/search.js
@@ -13,8 +13,26 @@ function search() {
     ).done(
         function (data) {
             data.forEach(function (movies) {
-                
-            });
+            $(".superman").append(
+               <article>
+                   <h2>Based on your seach we found : </h2>
+              <div>
+                Name: ${movies.movie}<br>
+                Description: ${movies.description}<br>
+                Genre : ${movies.genre}<br>
+                Price: ${movies.price}<br>
+                Release: ${movies.release}<br>
+                Language: ${movies.language}<br>
+              </div>
+               </article>
+            );
+            })
         }
     )
-}
+
+    .fail(
+        function (err) {
+        console.log(err.responseText);
+        }
+    )
+})
