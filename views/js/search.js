bee8fb6bf800f452ebc4716e138b2efae72061f0
LaxRav <65710248+LaxRav@users.noreply.github.com>
2021-01-26 11:29:22 +0800

    Update search.js

--- views/js/search.js
+++ views/js/search.js
@@ -66,42 +66,5 @@ $(".genre").on('click', function () {
 })
 
 
-function search() {
-  //  var urlParams = new URLSearchParams(window.location.search);
-   // movie = urlParams.get('movie');
-    var movies = {
-        movie: $("#search").val()
-    };
-    $.ajax(
-        {
-            url: "/movies/search",
-            method: 'post',
-            data: movies
-        }
-    ).done(
-        function (data) {
-            data.forEach(function (movies) {
-            $(".superman").append(`
-               <article>
-                   <h2>Based on your seach we found : </h2>
-              <div>
-                Name: ${movies.movie}<br>
-                Description: ${movies.description}<br>
-                Genre : ${movies.genre}<br>
-                Price: ${movies.price}<br>
-                Release: ${movies.release}<br>
-                Language: ${movies.language}<br>
-              </div>
-               </article>
-            `);
-            })
-        }
-    )
 
-    .fail(
-        function (err) {
-        console.log(err.responseText);
-        }
-    )
-}
 
