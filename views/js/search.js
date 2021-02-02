82027e6aa3acb656557c76e8ea741127b4517581
LaxRav <65710248+LaxRav@users.noreply.github.com>
2021-02-02 14:26:58 +0800

    changes

--- views/js/search.js
+++ views/js/search.js
@@ -1,68 +1,47 @@
-$(document).ready(function () {
+function search() {
+    var genre = {
+        genre: $("#genre").val()
+        
+    }
     $.ajax({
-        url: "/genre/search",
-        method: "POST"
+        url:"/genre/search",
+        method:"post",
+        data: genre
     })
-        .done(
-            function (data) {
-                data.forEach(function (movies) {
-                    $(".superman").append(`
-                    <article>
-                       <div>
-                           Genres that are available : ${movies.genre}
-                        </div>
+    .done(function(data){
+        data.forEach(function (movies) {
+            $(".superman").append(`
+            <h2>These are the movies  based on your preferred genre type: </h2>
+            <article>
+                <h2>${movies.movie}</h2>
+                <div>
+                   Description: ${movies.description}<br>
+                   Genre : ${movies.genre}<br>
+                    Price: ${movies.price}<br>
+                    Release: ${movies.release}<br>
+                    Language: ${movies.language}<br>
+                </div>
+                <button><a href="/addtocart?id=${movies._id}"> Order Movie </a><button>                       
 
-                    </article>
-                `);
-                })
-            }
-        )
-
-        .fail(
-            function (err) {
-                console.log(err.responseText);
-            }
-        )
-
-});
-
-$(document).ready(function () {
-    $.ajax({
-        url: "/movies/search",
-        method: "POST"
+            </article>
+        `);
+                    
     })
-        .done(
-            function (data) {
-                data.forEach(function (movies) {
-                    $(".luthor").append(`
-                    <article>
-                    <div>
-                        <h2>Title: ${movies.movie}</h2>
-                        
-                           Description: ${movies.description}<br>
-                         
-                       </div>
-                    </article>
-                `);
-                });
-            }
-        )
+    .fail(function(err){
+        console.log(err.responseText);
+    })
+    return false;
+},
 
-        .fail(
-            function (err) {
-                console.log(err.responseText);
-            }
-        )
+
 
         $(".genre").on('click', function () {
             $(".superman").show();
-        });
+        })
         
-         $(".movie").on('click', function () {
-            $(".luthor").show();
-        });
+       
 
-});
+
 
 
 
