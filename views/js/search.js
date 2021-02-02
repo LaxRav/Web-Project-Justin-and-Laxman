e8a8c4aa3eb4a7d5764754e6c75233f46499431d
Justin8183 <75157449+Justin8183@users.noreply.github.com>
2021-02-02 14:52:58 +0800

    Revert "changes"
    This reverts commit 82027e6aa3acb656557c76e8ea741127b4517581.

--- views/js/search.js
+++ views/js/search.js
@@ -1,47 +1,68 @@
-function search() {
-    var genre = {
-        genre: $("#genre").val()
-        
-    }
+$(document).ready(function () {
     $.ajax({
-        url:"/genre/search",
-        method:"post",
-        data: genre
+        url: "/genre/search",
+        method: "POST"
     })
-    .done(function(data){
-        data.forEach(function (movies) {
-            $(".superman").append(`
-            <h2>These are the movies  based on your preferred genre type: </h2>
-            <article>
-                <h2>${movies.movie}</h2>
-                <div>
-                   Description: ${movies.description}<br>
-                   Genre : ${movies.genre}<br>
-                    Price: ${movies.price}<br>
-                    Release: ${movies.release}<br>
-                    Language: ${movies.language}<br>
-                </div>
-                <button><a href="/addtocart?id=${movies._id}"> Order Movie </a><button>                       
+        .done(
+            function (data) {
+                data.forEach(function (movies) {
+                    $(".superman").append(`
+                    <article>
+                       <div>
+                           Genres that are available : ${movies.genre}
+                        </div>
 
-            </article>
-        `);
-                    
+                    </article>
+                `);
+                })
+            }
+        )
+
+        .fail(
+            function (err) {
+                console.log(err.responseText);
+            }
+        )
+
+});
+
+$(document).ready(function () {
+    $.ajax({
+        url: "/movies/search",
+        method: "POST"
     })
-    .fail(function(err){
-        console.log(err.responseText);
-    })
-    return false;
-},
+        .done(
+            function (data) {
+                data.forEach(function (movies) {
+                    $(".luthor").append(`
+                    <article>
+                    <div>
+                        <h2>Title: ${movies.movie}</h2>
+                        
+                           Description: ${movies.description}<br>
+                         
+                       </div>
+                    </article>
+                `);
+                });
+            }
+        )
 
-
+        .fail(
+            function (err) {
+                console.log(err.responseText);
+            }
+        )
 
         $(".genre").on('click', function () {
             $(".superman").show();
-        })
+        });
         
-       
+         $(".movie").on('click', function () {
+            $(".luthor").show();
+        });
 
-
+});
 
 
 
