effa61d5171a077decdcd0dc284471882f43ab56
Justin8183 <75157449+Justin8183@users.noreply.github.com>
2021-02-02 14:57:50 +0800

    pushing stuff

--- views/js/search.js
+++ views/js/search.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
     $.ajax({
+<<<<<<< HEAD
         url: "/genre/search",
         method: "POST"
+=======
+        url:"/genre/search",
+        method:"post",
+        data: genre
+    })
+    .done(function(data){
+        data.forEach(function (movies) {
+            $(".superman").append(`
+            <h2>These are the movies  based on your preferred genre type: </h2>
+            <article>
+                <h2>${movies.movie}</h2>
+                <div>
+                   Description: ${movies.description}<br>
+                   Genre : ${movies.genre}<br>
+                    Price: ${movies.price}<br>
+                    Release: ${movies.release}<br>
+                    Language: ${movies.language}<br>
+                </div>
+                <button><a href="/addtocart?id=${movies._id}"> Order Movie </a><button>                       
+
+            </article>
+        `);
+                    
+>>>>>>> parent of fd0e71f... search
     })
         .done(
             function (data) {
@@ -58,11 +83,17 @@ $(document).ready(function () {
             $(".superman").show();
         });
         
+<<<<<<< HEAD
          $(".movie").on('click', function () {
             $(".luthor").show();
         });
 
 });
+=======
+       
+
+
+>>>>>>> parent of fd0e71f... search
 
 
 
