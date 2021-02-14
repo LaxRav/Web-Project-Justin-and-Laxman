d9d762dd3503c5b9d8a3b885199ecc60612a5ed0
Justin8183 <75157449+Justin8183@users.noreply.github.com>
2021-02-14 21:50:13 +0800

    update

--- views/js/index.js
+++ views/js/index.js
@@ -156,3 +156,19 @@ function register() {
         })
     return false;
 }
+
+
+$(".logoutBtn").click(function(){
+    $.ajax({
+        url: "/logout?token="+sessionStorage.authToken,
+        method:"get"
+    })
+    .done(function(data){
+        sessionStorage.removeItem("authToken");
+        location.reload();
+        location.url("/index");
+    })
+    .fail(function(err){
+        console.log(err.responseText);
+    })
+});
